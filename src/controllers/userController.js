e5d9c144d1f732b106708ca8341f4372702c4fc1
TriadLc <mortistriad.lc@gmail.com>
2023-05-20 12:36:42 +0700

    Update 2023-05-20 NodeJs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,31 +1,48 @@
-import userService from '../services/userService';
+import userService from "../services/userService";
 
-let handleLoging = async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
+let handleLoggin = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
 
-    if (!email || !password) {
-        return res.status(500).json({
-            errCode: 1,
-            message: 'Missing inputs parameter!'
-        })
-    }
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing inputs parameter!",
+    });
+  }
 
-    let userData = await userService.handleUserLogin(email, password)
-    //check email exist
-    //password nhap vao ko dung
-    //return userInfor
-    // access_token :JWT json web token
+  let userData = await userService.handleUserLogin(email, password);
+  //check email exist
+  //password nhap vao ko dung
+  //return userInfor
+  // access_token :JWT json web token
 
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
+  });
+};
+
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id; //ALL, id
+
+  if (!id) {
     return res.status(200).json({
-        errCode: userData.errCode,
-        message: userData.errMessage,
-        user: userData.user ? userData.user : {}
-    })
-}
-
-
+      errCode: 1,
+      errMessage: "Missing required parameters",
+      users: [],
+    });
+  }
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users,
+  });
+};
 
 module.exports = {
-    handleLoging: handleLoging
-}
+  handleLoggin: handleLoggin,
+  handleGetAllUsers: handleGetAllUsers,
+};
