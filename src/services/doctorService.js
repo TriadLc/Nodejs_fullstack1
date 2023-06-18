dbece7fba58b9e8823f029f109a9874c9eea09b8
TriadLc <mortistriad.lc@gmail.com>
2023-06-18 11:12:34 +0700

    Update 23-06-18 cmt-1 Finish TopOutDoctor-slideba

---
+++ src/services/doctorService.js
@@ -0,0 +1,42 @@
+import db from "../models/index";
+
+let getTopDoctorHomeSerVice = (limitInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        limit: limitInput,
+        where: { roleId: "R2" },
+        //order: [["createAt", "DESC"]],
+        attributes: {
+          //exclude: ["password", "image"],
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueEn", "valueVi"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+
+      resolve({
+        errCode: 0,
+        data: users,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  getTopDoctorHomeSerVice: getTopDoctorHomeSerVice,
+};
